5a5e205b3605a62e26a8dd26e9ccfadaa9992348
jsjh160t <102368807+jsjh160t@users.noreply.github.com>
2023-06-24 16:39:13 +0800

    Update 3.js

--- 3/3.js
+++ 3/3.js
@@ -35,11 +35,54 @@
               console.log('Image uploaded:', data);
 
               // 顯示預覽圖片
-              const previewContainer = document.getElementById('previewContainer');
-              const imagePreview = document.createElement('img');
-              imagePreview.src = URL.createObjectURL(file);
-              previewContainer.appendChild(imagePreview);
-              preDiction(imagePreview); //進行預測
+              //const previewContainer = document.getElementById('previewContainer');
+              //const imagePreview = document.createElement('img');
+              //imagePreview.src = URL.createObjectURL(file);
+              //previewContainer.appendChild(imagePreview);
+
+              
+              //進行預測
+              // 宣告canvas 物件
+              const canvas1 = document.getElementById('canvas1');
+              // 宣告canvas 繪圖物件
+              var ctx = canvas1.getContext('2d')
+
+              // 分析結果文字物件
+              let header2 = document.getElementById("prediction");
+              let header4 = document.getElementById("score");
+  
+              // 先告js 影像物件
+              var myImage = new Image();
+              // 指定本地端範例影像路徑
+              myImage.src = URL.createObjectURL(file); 
+              // 一旦成功載入影像，觸發執行loadImage函式
+              myImage.addEventListener("load", loadImage, false);
+
+              // 觸發函式
+              async function loadImage(e) {
+                // 範例影像大小
+                img_width = myImage.width;
+                img_height = myImage.height;
+
+                // 調整canvas大小
+                canvas1.width = img_width;
+                canvas1.height = img_height;
+
+                // canvas 繪製底圖
+                ctx.drawImage(myImage, 0, 0, img_width, img_height);
+
+                // 讀取模型
+                mobilenet.load().then(model => {
+                  // 類別分析
+                  model.classify(myImage).then(predictions => {
+                    console.log(predictions)
+                    // 類別
+                    header2.innerText = predictions[0]['className'];
+                    // 分數
+                    header4.innerText = predictions[0]['probability'];
+                  });
+                });
+              
             })
             .catch(error => {
               console.error('Error uploading image:', error);
@@ -49,48 +92,4 @@
         reader.readAsDataURL(file);
       }
     }
-
-
-  function preDiction() {
-    // 宣告canvas 物件
-    const canvas1 = document.getElementById('canvas1');
-    // 宣告canvas 繪圖物件
-    var ctx = canvas1.getContext('2d')
-
-    // 分析結果文字物件
-    let header2 = document.getElementById("prediction");
-    let header4 = document.getElementById("score");
   
-    // 先告js 影像物件
-    var myImage = new Image();
-    // 指定本地端範例影像路徑
-    myImage.src = imagePreview; 
-    // 一旦成功載入影像，觸發執行loadImage函式
-    myImage.addEventListener("load", loadImage, false);
-
-    // 觸發函式
-    async function loadImage(e) {
-      // 範例影像大小
-      img_width = myImage.width;
-      img_height = myImage.height;
-
-      // 調整canvas大小
-      canvas1.width = img_width;
-      canvas1.height = img_height;
-
-      // canvas 繪製底圖
-      ctx.drawImage(myImage, 0, 0, img_width, img_height);
-
-      // 讀取模型
-      mobilenet.load().then(model => {
-        // 類別分析
-        model.classify(myImage).then(predictions => {
-          console.log(predictions)
-          // 類別
-          header2.innerText = predictions[0]['className'];
-          // 分數
-          header4.innerText = predictions[0]['probability'];
-        });
-      });
-    }    
-   }
