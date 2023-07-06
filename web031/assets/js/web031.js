e105647bf6d1e24c5b9625a1cd60a2afe62ddfdb
jsjh160t <102368807+jsjh160t@users.noreply.github.com>
2023-07-06 23:11:50 +0800

    Add files via upload

--- web031/assets/js/web031.js
+++ web031/assets/js/web031.js
@@ -5,7 +5,7 @@ const video = document.getElementById("video");
 const header2 = document.getElementById("prediction");
 const header4 = document.getElementById("score");
 
-// 啟動手機鏡頭
+// 啟動手機鏡頭並進行影像辨識
 async function startCamera() {
   try {
     // 取得手機鏡頭的視訊串流
@@ -16,7 +16,7 @@ async function startCamera() {
     
     // 等待視訊串流載入完成後再執行影像辨識
     video.addEventListener("loadedmetadata", () => {
-      loadImage(video);
+      loadImage();
     });
   } catch (error) {
     console.error("無法取得視訊串流:", error);
@@ -24,22 +24,11 @@ async function startCamera() {
 }
 
 // 觸發函式
-async function loadImage(image) {
-  // 範例影像大小
-  //const img_width = image.videoWidth;
-  //const img_height = image.videoHeight;
-
-  // 調整canvas大小
-  //canvas1.width = img_width;
-  //canvas1.height = img_height;
-
-  // canvas繪製底圖
-  //ctx.drawImage(image, 0, 0, img_width, img_height);
-
+async function loadImage() {
   // 讀取模型
   mobilenet.load().then(model => {
     // 類別分析
-    model.classify(image).then(predictions => {
+    model.classify(video).then(predictions => {
       console.log(predictions);
       // 類別
       header2.innerText = predictions[0]['className'];
