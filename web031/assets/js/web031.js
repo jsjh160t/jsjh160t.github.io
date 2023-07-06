339e26dd516a6f07c7d42f36ca299d5ff18f797e
jsjh160t <102368807+jsjh160t@users.noreply.github.com>
2023-07-06 23:23:57 +0800

    Update web031.js

--- web031/assets/js/web031.js
+++ web031/assets/js/web031.js
@@ -8,33 +8,41 @@ const header4 = document.getElementById("score");
 // 啟動手機鏡頭並進行影像辨識
 async function startCamera() {
   try {
+    // 取得手機鏡頭的視訊串流
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    
+    // 將視訊串流設定給video元素
     video.srcObject = stream;
-
+    
+    // 等待視訊串流載入完成後再執行影像辨識
     video.addEventListener("loadedmetadata", () => {
       // 確保視訊尺寸已經設定
       video.width = video.videoWidth;
       video.height = video.videoHeight;
       video.removeEventListener("loadedmetadata", arguments.callee);
       
-      loadImage();
+      // 開始進行影像辨識預測
+      predictLoop();
     });
   } catch (error) {
     console.error("無法取得視訊串流:", error);
   }
 }
 
-// 觸發函式
-async function loadImage() {
+// 影像辨識預測迴圈
+function predictLoop() {
   // 讀取模型
   mobilenet.load().then(model => {
-    // 類別分析
+    // 進行影像辨識預測
     model.classify(video).then(predictions => {
       console.log(predictions);
       // 類別
       header2.innerText = predictions[0]['className'];
       // 分數
       header4.innerText = predictions[0]['probability'];
+      
+      // 使用requestAnimationFrame進行下一輪預測
+      requestAnimationFrame(predictLoop);
     });
   });
 }
