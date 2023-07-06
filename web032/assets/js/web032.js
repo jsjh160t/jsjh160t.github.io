fdb10e746e603bc42a68e6f21560c0bed606431f
jsjh160t <102368807+jsjh160t@users.noreply.github.com>
2023-07-06 23:42:22 +0800

    Add files via upload

--- web032/assets/js/web032.js
+++ web032/assets/js/web032.js
@@ -1,93 +1,76 @@
-
-let video;
-let currentFacingMode = 'environment'; // 初始為後鏡頭
+// 宣告video物件
+const video = document.getElementById("video");
+// 宣告切換鏡頭按鈕
+const switchCameraBtn = document.getElementById("switchCameraBtn");
 
 // 分析結果文字物件
 const header2 = document.getElementById("prediction");
 const header4 = document.getElementById("score");
-      
-// Load the image model and setup the video
-async function init() {
-    // Convenience function to setup the video
-    video = document.createElement('video');
-    video.setAttribute('playsinline', '');
-    video.setAttribute('autoplay', '');
-    video.setAttribute('muted', '');
-    video.style.width = '100%';
-    document.getElementById("video-container").appendChild(video);
 
-    // Check if the user interacts with the page
-    //document.addEventListener('click', startVideo);  不用再按20230706
-    startVideo();
-    
+let currentCamera = "user"; // 目前使用的鏡頭
+
+// 啟動手機鏡頭並進行影像辨識
+async function startCamera() {
+  try {
+    // 取得手機鏡頭的視訊串流
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: currentCamera } });
+
+    // 將視訊串流設定給video元素
+    video.srcObject = stream;
+
+    // 等待視訊串流載入完成後再執行影像辨識
+    video.addEventListener("loadedmetadata", () => {
+      // 確保視訊尺寸已經設定
+      video.width = video.videoWidth;
+      video.height = video.videoHeight;
+      video.removeEventListener("loadedmetadata", arguments.callee);
+
+      // 開始進行影像辨識預測
+      predictLoop();
+    });
+  } catch (error) {
+    console.error("無法取得視訊串流:", error);
+  }
 }
 
-async function startVideo() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        // Get the available video sources (cameras)
-        const sources = await navigator.mediaDevices.enumerateDevices();
-        let videoSourceId = null;
-
-        // Find the video source ID of the desired facing mode
-        for (let source of sources) {
-            if (source.kind === 'videoinput') {
-                if (currentFacingMode === 'environment' && source.label.includes('back')) {
-                    videoSourceId = source.deviceId;
-                    break;
-                } else if (currentFacingMode === 'user' && source.label.includes('front')) {
-                    videoSourceId = source.deviceId;
-                    break;
-                }
-            }
-        }
-
-        // Constraints object to specify the video source
-        const constraints = {
-            video: {
-                deviceId: videoSourceId ? { exact: videoSourceId } : undefined,
-                facingMode: currentFacingMode
-            }
-        };
-
-        // Get the media stream using the constraints
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = stream;
-        await video.play();
-        document.removeEventListener('click', startVideo); // Remove the event listener after starting the video
-        window.requestAnimationFrame(loop);
-    }
+// 切換鏡頭
+async function switchCamera() {
+  currentCamera = currentCamera === "user" ? "environment" : "user";
+  await stopCamera();
+  startCamera();
 }
 
-async function loop() {
-    await predict();
-    window.requestAnimationFrame(loop);
+// 停止鏡頭
+async function stopCamera() {
+  const stream = video.srcObject;
+  const tracks = stream.getTracks();
+  tracks.forEach(track => track.stop());
+  video.srcObject = null;
 }
 
-// run the video frame through the image model
-async function predict() {
-    // predict can take in an image, video, or canvas HTML element
-    // 讀取模型
-        mobilenet.load().then(model => {
-          // 類別分析
-          model.classify(video).then(predictions => {
-            console.log(predictions);
-            // 類別
-            header2.innerText = predictions[0]['className'];
-            // 分數
-            header4.innerText = predictions[0]['probability'];
-          });
-        });
+// 影像辨識預測迴圈
+function predictLoop() {
+  // 讀取模型
+  mobilenet.load().then(model => {
+    // 進行影像辨識預測
+    model.classify(video).then(predictions => {
+      console.log(predictions);
+      // 類別
+      header2.innerText = predictions[0]['className'];
+      // 分數
+      header4.innerText = predictions[0]['probability'];
+
+      // 使用requestAnimationFrame進行下一輪預測
+      requestAnimationFrame(predictLoop);
+    });
+  });
 }
 
-// 切換前後鏡頭
-function switchCamera() {
-    if (currentFacingMode === 'environment') {
-        currentFacingMode = 'user'; // 切換為前鏡頭
-    } else {
-        currentFacingMode = 'environment'; // 切換為後鏡頭
-    }
-
-    // 重新啟動視訊
-    startVideo();
-}
-
+// 在網頁載入時啟動手機鏡頭
+window.addEventListener("DOMContentLoaded", () => {
+  startCamera();
+  // 綁定切換鏡頭按鈕的點擊事件
+  switchCameraBtn.addEventListener("click", () => {
+    switchCamera();
+  });
+});
