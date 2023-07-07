1348789f5803b4b10ae98afc040ee17286f95a53
jsjh160t <102368807+jsjh160t@users.noreply.github.com>
2023-07-07 19:19:51 +0800

    Update web03.js

--- web03/assets/js/web03.js
+++ web03/assets/js/web03.js
@@ -53,7 +53,8 @@
             // 類別
             header2.innerText = predictions[0]['className'];
             // 分數 20230707四捨五入至小數第二位
-            header4.innerText = predictions[0]['probability'].toFixed(2);
+            var numb = predictions[0]['probability'];
+            header4.innerText = numb.toFixed(2);
           });
         });
       }
